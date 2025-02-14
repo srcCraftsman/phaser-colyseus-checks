8e2eaea2c999aa6cce801144cafe84c0372668c6
Artyom Borissov <somebigelephant@gmail.com>
2025-02-14 16:59:12 +0100

    completed

--- client/index.ts
+++ client/index.ts
@@ -3,6 +3,8 @@ import { Client, Room } from "colyseus.js";
 
 // custom scene class
 export class GameScene extends Phaser.Scene {
+    currentPlayer: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+  remoteRef: Phaser.GameObjects.Rectangle;
      // local input cache
      inputPayload = {
         left: false,
@@ -35,23 +37,33 @@ export class GameScene extends Phaser.Scene {
         console.error(e);
       }
     // listen for new players
-this.room.state.players.onAdd((player, sessionId) => {
-    const entity = this.physics.add.image(player.x, player.y, 'ship_0001');
-
-    // keep a reference of it on `playerEntities`
-    this.playerEntities[sessionId] = entity;
-
-    // listening for server updates
-    player.onChange(() => {
-        // update local position immediately
-        entity.x = player.x;
-        entity.y = player.y;
+    this.room.state.players.onAdd((player, sessionId) => {
+        const entity = this.physics.add.image(player.x, player.y, 'ship_0001');
+        this.playerEntities[sessionId] = entity;
+    
+        if (sessionId === this.room.sessionId) {
+            // this is the current player!
+            // (we are going to treat it differently during the update loop)
+            this.currentPlayer = entity;
+    
+            // remoteRef is being used for debug only
+            this.remoteRef = this.add.rectangle(0, 0, entity.width, entity.height);
+            this.remoteRef.setStrokeStyle(1, 0xff0000);
+    
+            player.onChange(() => {
+                this.remoteRef.x = player.x;
+                this.remoteRef.y = player.y;
+            });
+    
+        } else {
+            // all remote players are here!
+            // (same as before, we are going to interpolate remote players)
+            player.onChange(() => {
+                entity.setData('serverX', player.x);
+                entity.setData('serverY', player.y);
+            });
+        }
     });
-
-    // Alternative, listening to individual properties:
-    // player.listen("x", (newX, prevX) => console.log(newX, prevX));
-    // player.listen("y", (newY, prevY) => console.log(newY, prevY));
-});
      // delete leaving players
     this.room.state.players.onRemove((player, sessionId) => {
         const entity = this.playerEntities[sessionId];
@@ -66,15 +78,43 @@ this.room.state.players.onAdd((player, sessionId) => {
     }
 
     update(time: number, delta: number): void {
-        // skip loop if not connected with room yet.
-        if (!this.room) { return; }
+    // skip loop if not connected yet.
+    if (!this.currentPlayer) { return; }
 
-        // send input to the server
-        this.inputPayload.left = this.cursorKeys.left.isDown;
-        this.inputPayload.right = this.cursorKeys.right.isDown;
-        this.inputPayload.up = this.cursorKeys.up.isDown;
-        this.inputPayload.down = this.cursorKeys.down.isDown;
-        this.room.send(0, this.inputPayload);
+    const velocity = 2;
+    this.inputPayload.left = this.cursorKeys.left.isDown;
+    this.inputPayload.right = this.cursorKeys.right.isDown;
+    this.inputPayload.up = this.cursorKeys.up.isDown;
+    this.inputPayload.down = this.cursorKeys.down.isDown;
+    this.room.send(0, this.inputPayload);
+
+    if (this.inputPayload.left) {
+        this.currentPlayer.x -= velocity;
+
+    } else if (this.inputPayload.right) {
+        this.currentPlayer.x += velocity;
+    }
+
+    if (this.inputPayload.up) {
+        this.currentPlayer.y -= velocity;
+
+    } else if (this.inputPayload.down) {
+        this.currentPlayer.y += velocity;
+    }
+
+    for (let sessionId in this.playerEntities) {
+        // do not interpolate the current player
+        if (sessionId === this.room.sessionId) {
+            continue;
+        }
+    
+        // interpolate all other player entities
+        const entity = this.playerEntities[sessionId];
+        const { serverX, serverY } = entity.data.values;
+    
+        entity.x = Phaser.Math.Linear(entity.x, serverX, 0.2);
+        entity.y = Phaser.Math.Linear(entity.y, serverY, 0.2);
+    }
     }
     
     
